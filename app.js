71b816479792f440e14f2aa905006278b0a6774a
“Brenno <brennoeudes@gmail.com>
2023-03-21 16:26:57 -0300

    ajuste cors e primeiras importações book.routes

--- app.js
+++ app.js
@@ -9,10 +9,16 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 // Não esquecer de criar variável de ambiente com o endereço do seu app React (local ou deployado no Netlify)
-app.use(cors({ origin: process.env.REACT_APP_URL }));
+app.use(cors());
 
 const userRouter = require("./routes/user.routes");
 app.use("/user", userRouter);
+
+/*
+const bookRouter = require("./routes/book.routes");
+app.use("/book", bookRouter);
+*/
+
 
 app.listen(Number(process.env.PORT), () =>
   console.log(`Server up and running at port ${process.env.PORT}`)
---
+++ routes/book.routes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+
+//const UserModel = require("../models/User.model");
+const generateToken = require("../config/jwt.config");
+const isAuthenticated = require("../middlewares/isAuthenticated");
+const attachCurrentUser = require("../middlewares/attachCurrentUser");
+
+
+
+module.exports = router;
